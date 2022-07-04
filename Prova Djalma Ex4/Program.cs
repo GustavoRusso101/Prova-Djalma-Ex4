e83e5425597dff7005992716b2ecbc74518b0ee8
Gustavo <Gustavo@GUSTAVO-PC>
2022-07-03 23:23:12 -0300

    Adicionar arquivos de projeto.

---
+++ Prova Djalma Ex4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_Djalma_Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n1 = 0; n1 < 51; n1++)
+            {
+                Console.WriteLine(n1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
